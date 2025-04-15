5d45e362c9196f03004feb5ae923d4fb155f95c1
SirJaey <smartjoshua98@gmail.com>
2025-04-15 02:22:27 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 	base: '/flashcards-react/',
-	plugins: [react()],
+  plugins: [react()],
+  build: {
+    cssTarget: 'chrome61',
+  }
 });
